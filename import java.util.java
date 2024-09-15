f3b0c3d4881129abe56e0bfffd7dd940f0d5a448
Mayara Roseno <mayararoseno14@gmail.com>
2024-09-14 22:18:15 -0300

    :construction:
    Modelo inicial do código

---
+++ import java.util.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class CalculadoraSimples {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Escolha duas operações: ");
+        System.out.println("1. Soma");
+        System.out.println("2. Subtração");
+        System.out.println("3. Multiplicação");
+        System.out.println("4. Divisão");
+
+        int operacao = scanner.nextInt();
+
+        System.out.print("Mayara: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Digite o segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        double resultado;
+
+        switch (operacao) {
+            case 1:
+                resultado = num1 + num2;
+                System.out.println("Resultado da soma: " + resultado);
+                break;
+            case 2:
+                resultado = num1 - num2;
+                System.out.println("Resultado da subtração: " + resultado);
+                break;
+            case 3:
+                resultado = num1 * num2;
+                System.out.println("Resultado da multiplicação: " + resultado);
+                break;
+            case 4:
+                if (num2 != 0) {
+                    resultado = num1 / num2;
+                    System.out.println("Resultado da divisão: " + resultado);
+                } else {
+                    System.out.println("Erro: Divisão por zero não é permitida.");
+                }
+                break;
+            default:
+                System.out.println("Operação inválida.");
+                break;
+        }
+
+        scanner.close();
+    }
+}
